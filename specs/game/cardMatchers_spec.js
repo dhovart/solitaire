d34570570a02d47149f01e9f5a3e0444462a1449
Denis Hovart <hello@denishovart.com>
2016-09-05 19:57:53 -0300

    New build

---
+++ specs/game/cardMatchers_spec.js
@@ -0,0 +1,10 @@
+/* eslint no-undef: "error" */
+/* global describe it expect */
+
+import cardsMatchers from '../../src/game/cardsMatchers';
+
+describe('cardMatchers', () => {
+  it('matches a card of lesser value and of opposite color with the last card of a tableau', () => {
+  });
+});
+
